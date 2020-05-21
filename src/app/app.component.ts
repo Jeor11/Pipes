b058548de5f88fff165e94f09a75f4f9ec5b5f41
joel <joelolguin0@gmail.com>
2020-05-20 19:50:12 -0500

    pipe json para mostrar data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-nombre = 'Peter Parker,';
-personajes = [ 'SpiderMan', 'Hulk', 'IronMan'];
-PI: number = Math.PI;
-Porcentaje = 0.234;
-Salario = 1234.52323;
+  nombre = 'Peter Parker,';
+  personajes = ['SpiderMan', 'Hulk', 'IronMan'];
+  PI: number = Math.PI;
+  Porcentaje = 0.234;
+  Salario = 1234.52323;
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 50,
+    direccion: {
+      calle: 'prima'
+    }
+  };
+
 }
