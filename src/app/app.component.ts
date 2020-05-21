90e90ce9cef5abac1b80b67fb0f73243450b733d
joel <joelolguin0@gmail.com>
2020-05-21 13:52:31 -0500

    asycronia pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,6 +12,10 @@ export class AppComponent {
   PI: number = Math.PI;
   Porcentaje = 0.234;
   Salario = 1234.52323;
+
+  valorPromesa = new Promise<string> ( (resolve) => {
+  setTimeout( () => {resolve ('llego la data'); } , 3500);
+  });
 
   heroe = {
     nombre: 'Logan',
