ec6027227d92fa4ef97b850ceed4ff46024f7aad
joel <joelolguin0@gmail.com>
2020-06-02 18:52:53 -0500

    dom seguro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombre = 'Peter Parker,';
-  nombre2 = 'JoEl OLguiN rAmIrEz';
-  personajes = ['SpiderMan', 'Hulk', 'IronMan'];
-  PI: number = Math.PI;
-  Porcentaje = 0.234;
-  Salario = 1234.52323;
-  Fecha: Date = new Date() ;
-  idioma =  'es';
 
-  valorPromesa = new Promise<string> ( (resolve) => {
+  nombre: string = 'Peter Parker,';
+  nombre2: string = 'JoEl OLguiN rAmIrEz';
+  personajes: string[] = ['SpiderMan', 'Hulk', 'IronMan'];
+  PI: number = Math.PI;
+  Porcentaje: number = 0.234;
+  Salario: number = 1234.52323;
+  Fecha: Date = new Date() ;
+  idioma: string =  'es';
+  videoUrl: string = 'https://www.youtube.com/embed/HIlG2NKnsVA';
+
+  valorPromesa = new Promise <string> ( (resolve) => {
   setTimeout( () => {resolve ('llego la data'); } , 3500);
   });
 
